9ea650487860767be4d3f8d7788f061c44d9ce1a
U-ADP-PC3\ADP <ADP@ADP-PC3.(none)>
2012-04-19 23:09:07 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '79abb188780b880525a9559fbfc1ada83b98c29b899d52162a32a6ac48e43c01efd7a02f5a6bdd371c19801530243e399437eea3118dc2c17ebad252d54b7b4c'
